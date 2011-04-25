2ad3736e54b3b695f8cc70ca1dca3565f9934e0a
Hector Virgen <hector.virgen@disney.com>
2011-04-25 11:53:21 -0700

    Improves table of contents.

--- zfdocs.user.js
+++ zfdocs.user.js
@@ -6,44 +6,55 @@
 // @description  Improves the documentation for Zend Framework
 // ==/UserScript==
 
-// a function that loads jQuery and calls a callback function when jQuery has finished loading
-function addJQuery(callback) {
-	var script = document.createElement("script");
-	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-	script.addEventListener('load', function() {
-		var script = document.createElement("script");
-		script.textContent = "(" + callback.toString() + ")();";
-		document.body.appendChild(script);
-	}, false);
-	document.body.appendChild(script);
-}
+(function(){
+    // Return early if not on Zend Framework's website.
+    if (!/framework.zend.com/.test(document.location)) {
+        return;
+    }
+    
+    // a function that loads jQuery and calls a callback function when jQuery has finished loading
+    var addJQuery = function(callback) {
+        var script = document.createElement("script");
+        script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+        script.addEventListener('load', function() {
+            var script = document.createElement("script");
+            script.textContent = "(" + callback.toString() + ")();";
+            document.body.appendChild(script);
+        }, false);
+        document.body.appendChild(script);
+    };
 
-// the guts of this userscript
-function main() {
-    $j = jQuery.noConflict(true);
-	$j(function($){
-		var manual = $('#manual-container');
-		var sections = manual.find('.section[id^="zend."]');
-		
-		if (0 == sections.length) {
-			return;
-		}
-		
-		var contents = $('<ul></ul>');
-		var container = $('<div class="section"></div>');
-		
-		container.append('<div class="info"><h1 class="title" id="table-of-contents">Table of Contents</h1></div>');
-		
-		sections.each(function() {
-			var section = $(this);
-			var id = section.attr('id');
-			contents.append('<li><a href="#' + id + '">' + id + '</a></li>');
-		});
-		
-		container.append(contents);
-		manual.find('hr:eq(0)').after(container);
-	});
-}
+    // the guts of this userscript
+    var main = function() {
+        jQuery(function($){
+            var manual = $('#manual-container');
+            var container = $('<div class="section"></div>');
+            container.append('<div class="info"><h1 class="title"><strong>Table of Contents</strong></h1></div>');
+            var toc = function(section, container) {
+                var ul = $('<ul></ul>');
+                section.children('.section[id^="zend."]').each(function(){
+                    var child = $(this);
+                    var li = $('<li></li>');
+                    var link = $('<a></a>');
+                    link.attr('href', '#' + child.attr('id'));
+                    link.html(child.find('h1.title:eq(0)').html());
+                    li.append(link);
+                    ul.append(li);
+                    toc(child, li);
+                });
+                if (ul.length) {
+                    ul.css({
+                        margin: '0 0 0 20px',
+                        padding: 0
+                    });
+                    container.append(ul);
+                }
+            };
+            toc(manual, container);
+            manual.find('hr:eq(0)').after(container);
+        });
+    };
 
-// load jQuery and execute the main function
-addJQuery(main);
+    // load jQuery and execute the main function
+    addJQuery(main);
+})();
